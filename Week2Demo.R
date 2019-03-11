3971d074c889e4a59a765ca9d68fec19f1d03374
matthewmcnulty <mattjoe182@gmail.com>
2019-03-11 12:09:17 +0000

    Second commit

--- Week2Demo.R
+++ Week2Demo.R
@@ -2,3 +2,57 @@ x <- 2
 x
 y <- 5
 y
+a <- b <- 7
+a
+rm(a)
+a
+x <- 5
+class(x)
+is.numeric(x)
+is.numeric(b)
+i <- 5L
+i
+is.integer(i)
+date1 <- as.Date("2019-03-08")
+date1
+class(date1)
+as.numeric(date1)
+class(date1)
+date2 <- as.POSIXct("2019-03-08 09:00")
+class(date2)
+date2
+as.numeric(date2)
+2 == 3
+2 != 3
+a <- c(10, 150, 30, 40, 55.6)
+a
+p <- c(1, 2, 4, 7)
+q <- c(5, 5, 5)
+p + q
+install.packages("swirl")
+library(swirl)
+swirl()
+Matthew
+1
+1
+1
+1
+5 + 7
+x <- 5 + 7
+x
+x
+y <- x - 3
+y
+z <- c(1.1, 9, 3.14)
+?c
+z
+c(z, 555, z)
+z * 2 + 100
+my_sqrt <- sqrt(z - 1)
+2
+my_sqrt
+my_div <- z / my_sqrt
+1
+my_div
+c(1, 2, 3, 4) + c(0, 10, 100)
+info()
